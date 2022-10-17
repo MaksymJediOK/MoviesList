1ca5efbcfb2eba452bdabfd0c0202722cb800962
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2022-10-17 22:01:25 +0300

    movies database update, tables relations

--- src/MoviesDb/MoviesDbContext.cs
+++ src/MoviesDb/MoviesDbContext.cs
@@ -5,9 +5,33 @@ namespace MoviesCore
 {
     public class MoviesDbContext : IdentityDbContext<User>
     {
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Movie>()
+                .HasMany(x => x.Genres)
+                .WithMany(x => x.Movies);
+
+            builder.Entity<Movie>()
+                .HasMany(x => x.Directors)
+                .WithMany(x => x.Movies);
+
+            builder.Entity<Movie>()
+                .HasMany(x => x.Actors)
+                .WithMany(x => x.Movies);
+            base.OnModelCreating(builder);
+        }
         public MoviesDbContext(DbContextOptions<MoviesDbContext> options)
             : base(options)
         {
+
         }
+
+    
+        public DbSet<Movie>? Movies { get; set; }
+        public DbSet<Genre>? Genres { get; set; }
+        public DbSet<Director>? Directors { get; set; }
+        public DbSet<PublisherCountry>? PublisherCountries { get; set; }
+        public DbSet<Actor>? Actors { get; set; }
     }
 }
